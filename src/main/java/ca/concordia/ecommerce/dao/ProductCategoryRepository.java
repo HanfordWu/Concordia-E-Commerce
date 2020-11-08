94059244da20b7d3417aa882312da281c7a060f4
HanfordWu <hanfordwu@gmail.com>
2020-11-08 13:49:26 -0500

    add bootstrap to npm, add home page html template

--- src/main/java/ca/concordia/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/ca/concordia/ecommerce/dao/ProductCategoryRepository.java
@@ -3,12 +3,14 @@ package ca.concordia.ecommerce.dao;
 import ca.concordia.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by Hanford Wu on 2020-11-07 11:20 p.m.
  */
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
--- src/main/java/ca/concordia/ecommerce/dao/ProductRepository.java
+++ src/main/java/ca/concordia/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,12 @@ package ca.concordia.ecommerce.dao;
 
 import ca.concordia.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by Hanford Wu on 2020-11-07 11:19 p.m.
  */
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
