5beece4fca6725ef10ca8394ec35280f3e339de1
HanfordWu <hanfordwu@gmail.com>
2020-11-08 10:10:24 -0500

    disable delete, put, post actions of http, add the configuration.

---
+++ src/main/java/ca/concordia/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package ca.concordia.ecommerce.config;
+
+import ca.concordia.ecommerce.entity.Product;
+import ca.concordia.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+/**
+ * Created by Hanford Wu on 2020-11-08 9:52 a.m.
+ */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+//      disable http methods for product: put, post, and delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+        //      disable http methods for product category: put, post, and delete.
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+    }
+}
